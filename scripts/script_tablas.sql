1da55772901e959400dd992cfe108ff54b685e8d
patriciabonaldy <bonaldypatricia@gmail.com>
2020-05-07 13:41:01 -0300

    se actualiza readme

--- scripts/script_tablas.sql
+++ scripts/script_tablas.sql
@@ -37,24 +37,24 @@ create table meli1.detalle_lote_site (
 
 
 -- Creamos comentarios a las columnas de las tablas creadas
-comment on column detalle_lote_site.id_lote is 'ID de lote conformado por numerico, pertenece a la PK.';
-comment on column detalle_lote_site.site is 'Siglas de acuerdo al pais MLA (Argentina), MLB (Brasil), etc, pertenece a la PK.';
-comment on column detalle_lote_site.id_item is 'ID del item, pertenece a la PK.';
-comment on column detalle_lote_site.start_time is 'Fecha de inicio.';
-comment on column detalle_lote_site.price is 'Valor del item con decimales.';
-comment on column detalle_lote_site.descripcion is 'Descripcion del item.';
-comment on column detalle_lote_site.nickname is 'Diminutivo para el item.';
-comment on column detalle_lote_site.name is 'Nombre del item.';
-comment on column detalle_lote_site.categoria is 'Categoria a la que pertenece.';
-comment on column lote_site.id_lote is 'ID de lote conformado por numerico.';
-comment on column lote_site.nombre_archivo is 'Nombre del archivo.';
-comment on column lote_site.encode is 'Formato del archivo ASCII, UFT-8, etc.';
-comment on column lote_site.extension is 'Extension que indica tipo de archivo CSV, JSON, etc.';
-comment on column lote_site.separador_columna is 'Separador de columnas coma, punto, etc.';
-comment on column lote_site.fecha_insercion is 'Fecha en que se inserto el registro en la base de datos.';
-comment on column lote_site.tiempo_insercion is 'Tiempo en segundos que tardo la insercion.';
-comment on column lote_site.estado is 'Indica el estado ACTIVE / NON ACTIVE.';
-comment on column lote_site.detalle is 'Detalle del item.';
+comment on column meli1.detalle_lote_site.id_lote is 'ID de lote conformado por numerico, pertenece a la PK.';
+comment on column meli1.detalle_lote_site.site is 'Siglas de acuerdo al pais MLA (Argentina), MLB (Brasil), etc, pertenece a la PK.';
+comment on column meli1.detalle_lote_site.id_item is 'ID del item, pertenece a la PK.';
+comment on column meli1.detalle_lote_site.start_time is 'Fecha de inicio.';
+comment on column meli1.detalle_lote_site.price is 'Valor del item con decimales.';
+comment on column meli1.detalle_lote_site.descripcion is 'Descripcion del item.';
+comment on column meli1.detalle_lote_site.nickname is 'Diminutivo para el item.';
+comment on column meli1.detalle_lote_site.name is 'Nombre del item.';
+comment on column meli1.detalle_lote_site.categoria is 'Categoria a la que pertenece.';
+comment on column meli1.lote_site.id_lote is 'ID de lote conformado por numerico.';
+comment on column meli1.lote_site.nombre_archivo is 'Nombre del archivo.';
+comment on column meli1.lote_site.encode is 'Formato del archivo ASCII, UFT-8, etc.';
+comment on column meli1.lote_site.extension is 'Extension que indica tipo de archivo CSV, JSON, etc.';
+comment on column meli1.lote_site.separador_columna is 'Separador de columnas coma, punto, etc.';
+comment on column meli1.lote_site.fecha_insercion is 'Fecha en que se inserto el registro en la base de datos.';
+comment on column meli1.lote_site.tiempo_insercion is 'Tiempo en segundos que tardo la insercion.';
+comment on column meli1.lote_site.estado is 'Indica el estado ACTIVE / NON ACTIVE.';
+comment on column meli1.lote_site.detalle is 'Detalle del item.';
 
 
 exit;
