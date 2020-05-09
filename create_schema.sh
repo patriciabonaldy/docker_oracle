91d45baf109bdb17edf8c3d4729780e928aab465
patriciabonaldy <bonaldypatricia@gmail.com>
2020-05-09 14:06:27 -0300

    se modifica script

--- create_schema.sh
+++ create_schema.sh
@@ -1,6 +1,8 @@
 #!/bin/bas
 #!/bin/bash
 
+
+mount_BD(){
 set -e
 export INSTALL=/
 echo `hostname -I|awk '{print $1}'` `hostname -s` `hostname` >> /etc/hosts
@@ -23,8 +25,7 @@ gosu oracle  bash -c "$ORACLE_HOME/bin/netca -silent -responseFile $ORACLE_HOME/
 
 echo "Configuring the TNS"
 sh $INSTALL/tns.sh
-chown oracle:oinstall $ORACLE_HOME/network/admin/tnsnames.ora
-
+chown oracle:oinstall $ORACLE_HOME/network/admin/tnsnames.ora    
 echo "Testing Database"
 gosu oracle bash<<EOF 
 export ORACLE_SID=ORCL18
@@ -43,4 +44,60 @@ echo "Cleaning up"
 rm -rf /tmp/*
 echo "DataBase Installed!!!"
 gosu oracle  bash
+exit
+}
 
+
+down_services(){
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
+export ORACLE_SID=ORCL18 >> /home/oracle/.bashrc
+export ORACLE_BASE=/u01/app/oracle >> /home/oracle/.bashrc
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1 >> /home/oracle/.bashrc
+export PATH=$ORACLE_HOME/bin:$PATH >> /home/oracle/.bashrc    
+gosu oracle bash<<EOF 
+export ORACLE_SID=ORCL18
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
+export PATH=$ORACLE_HOME/bin:$PATH
+echo "Bajando BD"
+sqlplus sys/Welcome_1@ORCL18 as sysdba
+SHUTDOWN NORMAL;
+EOF
+echo "DataBase stopped!!!"
+exit
+}
+
+start_services(){
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
+export ORACLE_SID=ORCL18 >> /home/oracle/.bashrc
+export ORACLE_BASE=/u01/app/oracle >> /home/oracle/.bashrc
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1 >> /home/oracle/.bashrc
+export PATH=$ORACLE_HOME/bin:$PATH >> /home/oracle/.bashrc      
+gosu oracle bash<<EOF 
+export ORACLE_SID=ORCL18
+export ORACLE_BASE=/u01/app/oracle
+export ORACLE_HOME=/u01/app/oracle/product/18.0.0/dbhome_1
+export PATH=$ORACLE_HOME/bin:$PATH
+sqlplus / as sysdba
+startup;
+EOF
+echo "DataBase started!!!"
+exit
+}
+
+
+case "$1" in
+    down_BD)
+        down_services
+        ;;
+    start_BD)
+        start_services
+        ;;
+    mount_BD)
+        mount_BD
+        ;;
+    *)
+        echo "Usage: $0 {down_BD|start_BD|mount_BD}"
+esac
--- run_docker.sh
+++ run_docker.sh
@@ -4,5 +4,11 @@ docker run -it -d -p 1521:1521  --name=meli bd_meli
 
 export DOCKER_ID=$(docker container ls | grep bd_meli | awk '{print $1}')
 
-#docker exec -it $DOCKER_ID  /bin/bash  ./post_install.sh 
-docker exec -it $DOCKER_ID  /bin/bash  ./create_schema.sh
+#Crea el usuario meli y sus tablas
+docker exec  $DOCKER_ID  /bin/bash  ./create_schema.sh  mount_BD
+##########################################################################
+## baja la BD (se aplica si se realizo cambio a nivel de performance)   ##
+# descomentar para que tome los cambios                                 ##
+#########################################################################
+#docker exec  $DOCKER_ID  /bin/bash  ./create_schema.sh  down_BD
+#docker exec  $DOCKER_ID  /bin/bash  ./create_schema.sh  start_BD
